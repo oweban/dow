b69983c3171cbc42de2366f4b91d5d309bed84f8
oweban <owe84n@gmail.com>
2022-10-19 14:47:39 +1100

    Create macos_installer_script.sh

---
+++ macos_installer_script.sh
@@ -0,0 +1,82 @@
+#!/bin/zsh
+
+##########################################################
+# Script for installing a bunch of things rapidly.       #
+#                                                        #
+# Put this file in /Users/Shared, then open Terminal     #
+#                                                        #
+# You can also get this file faster by ignoring putting  #
+# the file in place, and just doing the curl line below. #
+#                                                        #
+# sudo zsh                                               #
+# cd /Users/Shared                                       #
+#                                                        #
+# OPTIONAL FOR GETTING THIS FAST                         #
+#                                                         #
+#                                                        #
+# chmod +x ./macos_installer_script.sh                   #
+# ./macos_installer_script.sh                            #
+##########################################################
+
+
+# Let's get Installomator and make it executable
+curl https://raw.githubusercontent.com/Installomator/Installomator/main/Installomator.sh -o /Users/Shared/Installomator.sh
+chmod +x /Users/Shared/Installomator.sh
+
+# Now we need to edit DEBUG=1 to DEBUG=0 - search the file for it, and replace it wherever it happens, in case they change the line it's on.
+sed -i '' "s/DEBUG\=1/DEBUG\=0/" /Users/Shared/Installomator.sh
+
+# Install Chrome and Drive
+/Users/Shared/Installomator.sh googlechromepkg
+/Users/Shared/Installomator.sh googledrive
+
+# This is to bypass the Outlook/OneDrive/OneNote bullshit
+/Users/Shared/Installomator.sh microsoftautoupdate
+/Users/Shared/Installomator.sh microsoftexcel
+/Users/Shared/Installomator.sh microsoftword
+/Users/Shared/Installomator.sh microsoftpowerpoint
+
+# Get Zoom and TeamViewer, too
+/Users/Shared/Installomator.sh zoomclient
+/Users/Shared/Installomator.sh teamviewer
+
+# Get Creative Cloud?
+# Uncomment the following line if we want this
+#/Users/Shared/Installomator.sh adobecreativeclouddesktop
+
+
+# Let's make the teacher/education account #
+
+dscl . -create /Users/teacher
+dscl . -create /Users/teacher UserShell /bin/zsh
+dscl . -create /Users/teacher RealName "Teacher"
+dscl . -create /Users/teacher UniqueID "502"
+dscl . -create /Users/teacher PrimaryGroupID 20
+dscl . -create /Users/teacher NFSHomeDirectory /Users/teacher
+
+# Edit the following line to add in the current teacher password.
+
+dscl . -passwd /Users/teacher "password"
+dscl . -append /Groups/admin GroupMembership teacher
+dscl . -append /Users/teacher Picture /Library/User\ Pictures/Fun/Medal.tif
+
+
+######################################################################################
+# Below are some things I'm working on, so they're commented out for the time being. #
+######################################################################################
+
+# Enable right-click on apple magic mouse etc
+#/usr/bin/defaults write <PATH/TO/USER/HOMEFOLDER>/Library/Preferences/com.apple.driver.AppleHIDMouse.plist Button2 -int 2
+#
+# Show status bar in finder
+#/usr/bin/defaults write com.apple.finder ShowStatusBar -bool true
+#
+# Enable snap-to-grid for desktop icons
+#/usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
+#
+# Disable shadow in screenshots
+#defaults write com.apple.screencapture disable-shadow -bool true
+
+
+# Hide admin account?
+# Expanded Save dialog boxes?
